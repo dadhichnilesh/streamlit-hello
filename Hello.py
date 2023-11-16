1328271af2299c5e306600a4def592043c8ef46c
dadhichnilesh <150353507+dadhichnilesh@users.noreply.github.com>
2023-11-16 17:57:51 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,57 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# dashboard.py
 
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-LOGGER = get_logger(__name__)
+# Sample data
+data = pd.DataFrame({
+    'x': np.arange(100),
+    'y': np.random.randn(100)
+})
 
+# Title
+st.title('Sample Dashboard')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Line chart
+st.subheader('Line Chart')
+st.line_chart(data.set_index('x'))
 
-    st.write("# Welcome to Streamlit! 👋")
+# Scatter plot
+st.subheader('Scatter Plot')
+st.scatter_chart(data)
 
-    st.sidebar.success("Select a demo above.")
+# Histogram
+st.subheader('Histogram')
+st.bar_chart(data['y'])
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Data Table
+st.subheader('Data Table')
+st.dataframe(data)
 
+# Map
+st.subheader('Map')
+# You can customize the map based on your data
 
-if __name__ == "__main__":
-    run()
+# Sidebar
+st.sidebar.header('Settings')
+# Add widgets to the sidebar for interactive elements
+
+# You can add more interactive elements like sliders, checkboxes, etc. in the sidebar
+
+# Text input example
+user_input = st.sidebar.text_input("Enter some text:", "Type here...")
+
+# Slider example
+slider_value = st.sidebar.slider("Select a range", 0, 100, (25, 75))
+
+# Button example
+if st.sidebar.button('Submit'):
+    st.sidebar.success('Clicked!')
+
+# Footer
+st.markdown('---')
+st.markdown('Built with Streamlit')
+
+# Run the app with `streamlit run dashboard.py` in the terminal
